3d5b625aba4b734bb4683c20de154c08987cfbd0
Alex Kerfoot <alex@napofearth.com>
2014-03-31 04:52:14 -0700

    Fixing namespace bug

--- process.js
+++ process.js
@@ -29,7 +29,7 @@ THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
 OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-test = test || {};
+var test = test || {};
 test.audio = test.audio || {};
 
 (function() {
